4e430bbf04e241bae4f17b5a23c64bfbcf7c48a7
Dante de Araujo <danteeng@hotmail.com>
2023-06-26 11:13:18 -0300

    Adicionando view interface e nossa view do console

--- eddb/view/console.py
+++ eddb/view/console.py
@@ -1,37 +1,63 @@
+import time
+from colorama import init,Back, Fore
 from view.iview import IView
-import inspect
-from colorama import init,Back, Fore, Style
-from util.util import *
+from util.util import clear_screen
 
 init(autoreset=True)
 
 class Console(IView):
-    def __init__(self,initial_option):
-        self.current_options = initial_option
 
-    def show(self):
-        resposta = 0
-        while self.current_options != -1:
-            clear_screen();
-            self.printMenu("Sistema de Biblioteca",self.current_options)
-            resposta = int(input("Qual opção vc deseja?\n"))
-            self.handleOptions(resposta)
+    def show(self,menu):
+        self.printMenu("Sistema de Biblioteca", menu)
 
-    def printMenu(self,title,options):
+    def sair(self):
+        self.mostrar_mensagem("=" * (len("Até logo!")+6))
+        self.mostrar_mensagem("|| {} ||".format("Até logo!"))
+        self.mostrar_mensagem("=" * (len("Até logo!")+6))
+        time.sleep(2)
+        clear_screen()
+
+    def get_input_usuario(self,texto):
+        return input(texto)
+
+    def printMenu(self,title,menu):
         # print(Style.BRIGHT + Back.BLUE + Fore.WHITE + "=" * (len(title)+6))
         # print(Style.BRIGHT + Back.BLUE + Fore.WHITE + "|| {} ||".format(title))
         # print(Style.BRIGHT + Back.BLUE + Fore.WHITE + "=" * (len(title)+6))
         print("=" * (len(title)+6))
         print("|| {} ||".format(title))
         print("=" * (len(title)+6))
-        self.printOptions(options)
+        # Alinhado na direita
+        # print("=" * (len(title)+6 - (len(menu["titulo"]))), end='')
+        # TODO: Ajeitar o tamanho de forma genérica
+        # para que o texto fique centralizado
+        print("=" * (len(menu["titulo"])+2), end='')
+        print(f" {menu['titulo']} ", end='')
+        print("=" * (len(menu["titulo"])+5))
+        self.printOptions(menu["opções"])
 
     def printOptions(self,op):
         for i in range(len(op)):
             print("{}. {}".format(i+1,op[i]["entry"]))
             # print(Back.YELLOW + Fore.BLACK + "{}. {}".format(i+1,op[i]["entry"]))
 
-    def handleOptions(self,resp):
-        self.current_options = self.current_options[resp-1]["out_opt"]
-        if inspect.isfunction(self.current_options):
-            self.current_options = self.current_options()
+    def mostrar_mensagem(self,msg):
+        print(msg)
+
+    def mostrar_detalhes_livros(self,item):
+        title = "Detalhes Livro"
+        attrb = item.__dict__
+        print("=" * (len(title)+6))
+        print(f"|| {title} ||")
+        print("=" * (len(title)+6))
+        for k,v in attrb.items():
+            print(f"{k.capitalize()}: {v}")
+
+    def mostrar_detalhes_usuario(self,item):
+        title = "Detalhes Usuário"
+        print("=" * (len(title)+6))
+        print("|| {} ||".format(title))
+        print("=" * (len(title)+6))
+        print(f"Nome: {item.nome}")
+        print(f"Autor: {item.autor}")
+
--- eddb/view/iview.py
+++ eddb/view/iview.py
@@ -4,5 +4,13 @@ from abc import ABC,abstractmethod
 class IView(ABC):
 
     @abstractmethod
-    def show(self):
+    def show(self,menu):
+        pass
+
+    @abstractmethod
+    def mostrar_mensagem(self,msg):
+        pass
+
+    @abstractmethod
+    def sair(self):
         pass
