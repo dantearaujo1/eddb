d9de0d3b2cdcf470edd62d8cfabc0ca493a7ff98
Dante de Araujo <danteeng@hotmail.com>
2023-06-26 11:19:56 -0300

    Modificando main para conter o repositorio e a view e os menus

--- eddb/main.py
+++ eddb/main.py
@@ -2,6 +2,8 @@
 # from textual.widgets import Header, Footer
 import time
 from view.console import Console
+from controller.biblioteca_controller import BibliotecaController
+from repository.livros_repository import LivrosRepositoryJSON
 
 # class Console(App):
 #
@@ -11,41 +13,60 @@ from view.console import Console
 #         yield Header(name="Biblioteca")
 #         yield Footer()
 
-if __name__ == "__main__":
+
+def main():
     cadastro_opt = []
     livros_opt = []
     emprestimos_opt = []
 
-    inicio_opt= [
-        {"entry":"Menu Cadastro", "out_opt":cadastro_opt},
-        {"entry":"Menu Livros", "out_opt":livros_opt},
-        {"entry":"Menu Empréstimos", "out_opt":emprestimos_opt},
-        {"entry":"Sair", "out_opt":-1},
+    inicio_opt = [
+        {"entry": "Menu Livros", "out_opt": livros_opt, "próximo": 4},
+        {"entry": "Menu Empréstimos", "out_opt": emprestimos_opt, "próximo": 5},
+        {"entry": "Menu Cadastro", "out_opt": cadastro_opt, "próximo": 2},
+        {"entry": "Sair", "out_opt": -1, "próximo": -1},
+    ]
+    cadastro_opt.append({"entry": "Cadastrar Aluno", "próximo": 6})
+    cadastro_opt.append({"entry": "Editar Aluno", "próximo": 7})
+    cadastro_opt.append({"entry": "Apagar Aluno", "próximo": 8})
+    cadastro_opt.append({"entry": "Cadastrar Livro", "próximo": 9})
+    cadastro_opt.append({"entry": "Editar Livro", "próximo": 10})
+    cadastro_opt.append({"entry": "Apagar Livro", "próximo": 11})
+    cadastro_opt.append({"entry": "Voltar", "próximo": 0})
+    cadastro_opt.append({"entry": "Sair", "próximo": -1})
+
+    livros_opt.append({"entry": "Procurar Livro", "próximo": 12})
+    livros_opt.append({"entry": "Ver Livros", "próximo": 13})
+    livros_opt.append({"entry": "Voltar", "próximo": 0})
+    livros_opt.append({"entry": "Sair", "próximo": -1})
+
+    emprestimos_opt.append({"entry": "Buscar Empréstimo", "próximo": 14})
+    emprestimos_opt.append({"entry": "Ver empréstimos de Aluno", "próximo": 15})
+    emprestimos_opt.append({"entry": "Ver empréstimo de Livro", "próximo": 16})
+    emprestimos_opt.append({"entry": "Voltar", "próximo": 0})
+    emprestimos_opt.append({"entry": "Sair", "próximo": -1})
+
+    menus = [
+        {"titulo": "Inicio", "opções": inicio_opt},
+        {"titulo": "Cadastros", "opções": cadastro_opt},
+        {"titulo": "Alunos", "opções": inicio_opt},
+        {"titulo": "Livros", "opções": livros_opt},
+        {"titulo": "Empréstimo", "opções": emprestimos_opt},
     ]
 
-    def cadastrar():
-        print("Cadastrei!")
-        time.sleep(2)
-        return inicio_opt
+    repositorio_de_livros = LivrosRepositoryJSON("eddb/livros.json")
+    view = Console()
+    biblioteca = BibliotecaController(view, repositorio_de_livros, menus)
 
-    cadastro_opt.append({"entry":"Cadastrar Aluno", "out_opt":cadastrar})
-    cadastro_opt.append({"entry":"Editar Aluno", "out_opt":inicio_opt})
-    cadastro_opt.append({"entry":"Apagar Aluno", "out_opt":inicio_opt})
-    cadastro_opt.append({"entry":"Cadastrar Livro", "out_opt":inicio_opt})
-    cadastro_opt.append({"entry":"Editar Livro", "out_opt":inicio_opt})
-    cadastro_opt.append({"entry":"Apagar Livro", "out_opt":inicio_opt})
-    cadastro_opt.append({"entry":"Voltar", "out_opt":inicio_opt})
-    cadastro_opt.append({"entry":"Sair", "out_opt":-1})
+    livros_opt[1]["próximo"] = biblioteca.ver_livros
+    livros_opt[0]["próximo"] = biblioteca.procurar_livros
+    biblioteca.iniciar()
 
-    livros_opt.append({"entry":"Procurar Livro", "out_opt":inicio_opt})
-    livros_opt.append({"entry":"Ver Livros", "out_opt":inicio_opt})
-    livros_opt.append({"entry":"Voltar", "out_opt":inicio_opt})
-    livros_opt.append({"entry":"Sair", "out_opt":-1})
 
-    emprestimos_opt.append({"entry":"Buscar Empréstimo", "out_opt":inicio_opt})
-    emprestimos_opt.append({"entry":"Ver empréstimos de Aluno", "out_opt":inicio_opt})
-    emprestimos_opt.append({"entry":"Ver empréstimo de Livro", "out_opt":inicio_opt})
-    emprestimos_opt.append({"entry":"Voltar", "out_opt":inicio_opt})
-    emprestimos_opt.append({"entry":"Sair", "out_opt":-1})
-    app = Console(inicio_opt)
-    app.show()
+def cadastrar():
+    print("Cadastrei!")
+    time.sleep(2)
+    return inicio_opt
+
+
+if __name__ == "__main__":
+    main()
