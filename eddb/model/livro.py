6d4cb25f1b163881cfd70b58e6f2e2c0f45514df
Dante de Araujo <danteeng@hotmail.com>
2023-06-28 19:36:14 -0300

    Atualizando os dois models para serem passiveis de serialização em JSON

--- eddb/model/livro.py
+++ eddb/model/livro.py
@@ -1,8 +1,17 @@
 import uuid
 from dataclasses import dataclass, field
+from util.util import str_uuid4
+
 
 @dataclass
 class Livro:
-    id: [] = field(default_factory=uuid.uuid4)
+    id: str = field(default_factory=str_uuid4)
     nome: str = ""
     autor: str = ""
+
+    def __dict__(self):
+        return {
+            "id":f"{self.id}",
+            "titulo":f"{self.nome}",
+            "autor":f"{self.autor}",
+        }
--- eddb/model/usuario.py
+++ eddb/model/usuario.py
@@ -1,11 +1,21 @@
 import uuid
-from dataclasses import dataclass
+from dataclasses import dataclass, field
+from util.util import str_uuid4
 
 
 @dataclass
 class Usuario:
     """DocString"""
 
-    uuid: str = str(uuid.uuid4())
-    nome: str = ""
+    id: str = field(default_factory=str_uuid4)
+    nome: str = "User"
+    sobrenome: str = ""
+    matricula: int = 0
 
+    def __dict__(self):
+        return {
+            "id":f"{self.id}",
+            "nome":f"{self.nome}",
+            "sobrenome":f"{self.sobrenome}",
+            "matrícula":self.sobrenome,
+        }
