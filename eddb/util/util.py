045bd23782def89c50b88d49b4842cfaf860190b
Dante de Araujo <danteeng@hotmail.com>
2023-06-28 19:23:00 -0300

    Adicionando funções utilitárias str_uuid, write_data, etc

--- eddb/util/util.py
+++ eddb/util/util.py
@@ -1,10 +1,16 @@
+import uuid
 import os
 import json
 
+def str_uuid4():
+    return str(uuid.uuid4())
+
 def clear_screen():
     print("\033c", end='')
+
 def move_cursor(x,y):
-    print(f"\033[{y};{x}H", end='')
+    print(f"\033[{y};{x}H")
+
 def get_terminal_size():
     return os.get_terminal_size()
 
@@ -17,3 +23,13 @@ def load_data(file):
     with open_json(file) as data:
         print(data)
 
+def write_data(file,data):
+    with open(file, 'w') as f:
+        # pass
+        # d = json.dumps(data)
+        json.dump(data,f,indent=2,ensure_ascii=False)
+    #     print()
+
+def get_root(file):
+    path = os.path.dirname(os.path.abspath(file))
+    return path
