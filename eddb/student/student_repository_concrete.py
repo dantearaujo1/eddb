165f338f0ef61873900b21e82fd46d9dc6e9a9ad
debisameque <debisameque@gmail.com>
2023-06-29 20:52:37 -0300

    Implementei a interface StudentRepository na StudentRepositoryConcrete e ajeitei as chaves do json

--- eddb/student/student_repository_concrete.py
+++ eddb/student/student_repository_concrete.py
@@ -1,4 +1,21 @@
+from eddb.student.student_interface.student_repository import StudentRepository
+from eddb.util.util import open_json,write_data
+from eddb.model.usuario import Usuario
+class StudentRepositoryConcrete(StudentRepository):
+    def __init__(self, file):
+        self.file = file
 
-class StudentRepositoryConcrete:
-    def __init__(self):
-        pass
+    def __open(self):
+        json_data = None
+        if self.file:
+            json_data = open_json(self.file)
+        return json_data
+    """
+       Return a list of dictionary objects that with data
+       """
+
+    def get_all(self):
+        json_data = self.__open()
+        if json_data:
+            return json_data["students"]
+        return []
