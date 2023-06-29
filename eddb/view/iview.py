5ed48ae2d58967a2e2acafbbc85abc321422550b
Dante de Araujo <danteeng@hotmail.com>
2023-06-29 16:20:13 -0300

    Adicionando Views

--- eddb/view/iview.py
+++ eddb/view/iview.py
@@ -4,19 +4,10 @@ from abc import ABC,abstractmethod
 class IView(ABC):
 
     @abstractmethod
-    def show(self,menu) -> None:
+    def show(self,data) -> None:
         pass
 
     @abstractmethod
-    def get_input(self) -> str:
+    def sair(self,length=5) -> int:
         pass
 
-    @abstractmethod
-    def sair(self) -> int:
-        pass
-
-    def run(self) -> None:
-        while(True):
-            self.show()
-            self.get_input()
-        self.sair()
