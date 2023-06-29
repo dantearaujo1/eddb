b80463e0e1928edf24f38afd06c80f943db3d91b
Dante de Araujo <danteeng@hotmail.com>
2023-06-29 19:53:48 -0300

    Adicionando getAll no repo

--- eddb/book/book_repository_concrete.py
+++ eddb/book/book_repository_concrete.py
@@ -1,4 +1,31 @@
+"""
+    Class that will handle our data management using JSON as the stored
+    File
+"""
+from eddb.util.util import open_json,write_data
+from eddb.book.book_interface.book_repository import BookRepository
+from eddb.model.livro import Livro
 
-class BookRepositoryConcrete:
-    def __init__(self):
-        pass
+class BookRepositoryConcrete(BookRepository):
+    def __init__(self,file):
+        self.books = {}
+        self.file = file
+        # if file:
+        #     dados = open_json(file)
+        #     for dado_livro in dados["livros"]:
+        #         dados_do_livro = dado_livro
+        #         livro = Livro(id=dados_do_livro["id"],nome=dados_do_livro["titulo"],autor=dados_do_livro["autor"])
+        #         self.livros[livro.id] = livro
+
+
+
+    """
+    Return a list of dictionary objects that with data
+    """
+    def get_all(self):
+        if self.file:
+            json_data = open_json(self.file)
+            return json_data["livro"]
+        return []
+            # for livro in dados["livros"]:
+            #     livro_obj = Livro(id=livro["id"],nome=livro["titulo"],autor=livro["autor"])
