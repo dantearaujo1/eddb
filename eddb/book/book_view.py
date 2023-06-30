2db8d2c60aa9e042f7b506b7288f9024f779c576
Dante de Araujo <danteeng@hotmail.com>
2023-06-29 21:17:22 -0300

    Iniciando a implementação da bookview

--- eddb/book/book_view.py
+++ eddb/book/book_view.py
@@ -1,12 +1,34 @@
+from readchar import readkey, key
+from colorama import Back,Fore
+
 from eddb.book.book_interface.book_controller import BookController
 from eddb.book.book_interface.feedback_book_view import FeedbackBookView
+
 class BookView(FeedbackBookView):
     def __init__(self, controller: BookController):
         self.controller = controller
+        self.options = ["Procurar","Cadastrar","Editar","Excluir","Sair"]
+        self.option = 0
+
     def show_books(self, books):
         for book in books:
             print(book.name)
-        pass
+
+    def show_menu(self):
+        for i in range(len(self.options)):
+            if i == self.option:
+                print(f"{Back.WHITE}{self.options[i]}")
+            else:
+                print(f"{self.options[i]}")
+
+
+    def get_input(self):
+        k = readkey()
+        if k in (key.CTRL_N,key.CTRL_J):
+            self.option += 1 % len(self.options)
+        if k in (key.Return):
+            print("Escolher")
+
 
     def start(self):
         pass
