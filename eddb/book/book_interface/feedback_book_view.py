b1226c4e94b58f3bf14ec9584d556d1624c2b7c7
Dante de Araujo <danteeng@hotmail.com>
2023-06-30 12:48:58 -0300

    Adicionando algumas feedbacksView de sucesso e falha

--- eddb/book/book_interface/feedback_book_view.py
+++ eddb/book/book_interface/feedback_book_view.py
@@ -1,5 +1,21 @@
 from abc import ABC,abstractmethod
+import time
 class FeedbackBookView(ABC):
     @abstractmethod
     def show_books(self, books):
         pass
+
+
+class SuccessFeedbackAddBookView(FeedbackBookView):
+
+    def show_books(self,books):
+        print(f"Parabéns, você adicionou o livro: ")
+        print(f"Título: {books[0].title}")
+        print(f"Autor: {books[0].author}")
+        time.sleep(3)
+
+class FailureFeedbackAddBookView(FeedbackBookView):
+
+    def show_books(self,books):
+        print(f"Não foi possível adicionar o livro!")
+        time.sleep(3)
