567e2fef6d6c584144338fa5b5edc612416d0145
Dante de Araujo <danteeng@hotmail.com>
2023-06-30 14:44:18 -0300

    Mesma coisa dos outros db_teste.json só que para o teste

--- tests/test_livro_repository.py
+++ tests/test_livro_repository.py
@@ -2,7 +2,7 @@ from repository.livros_repository import LivrosRepositoryJSON
 
 
 def test_add_livro():
-    l = LivrosRepositoryJSON("eddb/livros_test.json")
+    l = LivrosRepositoryJSON("eddb/db_teste.json")
     livros = l.get_all()
     print(livros)
     assert True
