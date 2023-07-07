27bf8c4cbb6204d83b6e24a3f9c9d61bd101ab79
Dante de Araujo <danteeng@hotmail.com>
2023-07-07 10:45:18 -0300

    Corrigindo bug da opção reversa

--- eddb/util/menus.py
+++ eddb/util/menus.py
@@ -14,26 +14,34 @@ def draw_scrollable_menu(items,fake,ini_item,reverse=False,other_lines=1):
     total_items = len(items)
     padding = 1
     borders = {"right":theme["border_right"],"left":theme["border_left"],"up":theme["border_up"],"down":theme["border_down"]}
+
     clear_screen()
     move_cursor(0,window_vertical_size-total_items-2)
-    if end_item_position < total_items and (ini_item + menu_vertical_size) < total_items - 1:
-        print(f"{theme['scrollable_up_arrow_on']}^{Style.RESET_ALL}{(window_horizontal_size-4)*borders['up']}")
-    else:
-        print(f"{theme['scrollable_up_arrow_off']}^{Style.RESET_ALL}{(window_horizontal_size-4)*borders['up']}")
-
     if reverse:
-        fake_selected = len(items)-fake-1
-        for i in range(len(items)):
+        if ini_item > 0:
+            print(f"{theme['scrollable_up_arrow_on']}^{Style.RESET_ALL}{(window_horizontal_size-4)*borders['up']}")
+        else:
+            print(f"{theme['scrollable_up_arrow_off']}^{Style.RESET_ALL}{(window_horizontal_size-4)*borders['up']}")
+
+        for i in range(menu_vertical_size):
             if total_items > 0:
                 if i // total_items > 0:
                     break
-                item = items[len(items)-i-1]
-                move_cursor(0,menu_vertical_size-i+1)
+                item = items[ini_item+i]
                 if fake_selected == i:
                     print(f"{padding*borders['left']}{theme['bselected']}{theme['fselected']}{item}")
                 else:
                     print(f"{padding*borders['left']}{item}")
+        move_cursor(0,window_vertical_size-1)
+        if (ini_item + menu_vertical_size) < total_items:
+            print(f"{theme['scrollable_up_arrow_on']}v{Style.RESET_ALL}{(window_horizontal_size-4)*borders['down']}")
+        else:
+            print(f"{theme['scrollable_up_arrow_off']}v{Style.RESET_ALL}{(window_horizontal_size-4)*borders['down']}")
     else:
+        if end_item_position < total_items and (ini_item + menu_vertical_size) < total_items - 1:
+            print(f"{theme['scrollable_up_arrow_on']}^{Style.RESET_ALL}{(window_horizontal_size-4)*borders['up']}")
+        else:
+            print(f"{theme['scrollable_up_arrow_off']}^{Style.RESET_ALL}{(window_horizontal_size-4)*borders['up']}")
         for i in range(menu_vertical_size):
             if total_items > 0:
                 if i // total_items > 0:
@@ -44,11 +52,11 @@ def draw_scrollable_menu(items,fake,ini_item,reverse=False,other_lines=1):
                     print(f"{padding*borders['left']}{theme['bselected']}{theme['fselected']}{item}")
                 else:
                     print(f"{padding*borders['left']}{item}")
-    move_cursor(0,window_vertical_size-1)
-    if ini_item > 0:
-        print(f"{theme['scrollable_up_arrow_on']}v{Style.RESET_ALL}{(window_horizontal_size-4)*borders['down']}")
-    else:
-        print(f"{theme['scrollable_up_arrow_off']}v{Style.RESET_ALL}{(window_horizontal_size-4)*borders['down']}",end="")
+        move_cursor(0,window_vertical_size-1)
+        if ini_item > 0:
+            print(f"{theme['scrollable_up_arrow_on']}v{Style.RESET_ALL}{(window_horizontal_size-4)*borders['down']}")
+        else:
+            print(f"{theme['scrollable_up_arrow_off']}v{Style.RESET_ALL}{(window_horizontal_size-4)*borders['down']}")
 
 
 
